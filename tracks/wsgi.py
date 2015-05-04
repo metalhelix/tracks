0957ef95bf119e45b9084f3cd36e3bd8b52426c2
Malcolm Cook <malcolm_cook@stowers.org>
2015-05-04 12:28:53 -0500

    fix initial production install url dispatch bug

--- tracks/wsgi.py
+++ tracks/wsgi.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracks.settings")
+#os.environ.setdefault("PRODUCTION", "1") ## opting to inherit this from either USER environment or /etc/init.d/uwsgi.
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
