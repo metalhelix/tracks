535cdbe6e5ade881906591718b77c902e6664ba8
Malcolm Cook <malcolm_cook@stowers.org>
2015-05-07 16:28:58 -0500

    remove reference to deployment path; instead, indirect through __file__

--- tracks/wsgi.py
+++ tracks/wsgi.py
@@ -7,9 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))) ## i.e. sys.path.append('/microarray/tracks/')
+#os.environ.setdefault("PRODUCTION", "1") # PRODUCTION is best set in calling environment, i.e. in service configuration for uwsgi
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracks.settings")
-#os.environ.setdefault("PRODUCTION", "1") ## opting to inherit this from either USER environment or /etc/init.d/uwsgi.
+#os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tracks.settings.production")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
